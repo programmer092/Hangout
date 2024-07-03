12dcd2edf014f2a705f5cd80e25b9781e2cd781d
Bikash Bohara <bikashbohara092@gmail.com>
2024-07-03 14:53:56 +0545

    configured Cloudinary

--- server/src/config/multerProfile.ts
+++ server/src/config/multerProfile.ts
@@ -3,6 +3,11 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import { Request } from "express";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
